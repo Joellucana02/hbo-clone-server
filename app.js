34959513e33a34d754f98c4846b0e2215a23c624
joel <joellucana02@gmail.com>
2021-08-19 18:48:12 -0300

    added routing to app

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-
+//importing routes-->
+const usersRouter = require("./routes/usersRouter");
+const authRouter = require("./routes/authRouter");
+const postsRouter = require("./routes/postsRouter");
 //middlewares-->
 app.use(express.json());
-//routes-->
-app.use("api/v1/users", someUserRoute);
+
+//routing pages-->
+app.use("/api/v1", authRouter);
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/users", usersRouter);
 
 module.exports = app;
